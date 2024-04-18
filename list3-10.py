3e284163c4d95ba0a254a5e9f950ee2c32e9eb4e
Eliabe77 <158612069+Eliabe77@users.noreply.github.com>
2024-04-18 17:31:47 +0200

    Add files via upload

---
+++ list3-10.py
@@ -0,0 +1,37 @@
+oggettixvaligia = ['vestiti', 'nessessaire', 'pc', 'sigarette']
+
+print("Original order:")
+print(oggettixvaligia)
+
+print("\nAlphabetical:")
+print(sorted(oggettixvaligia))
+
+print("\nOriginal order:")
+print(oggettixvaligia)
+
+print("\nReverse alphabetical:")
+print(sorted(oggettixvaligia, reverse=True))
+
+print("\nOriginal order:")
+print(oggettixvaligia)
+
+print("\nReversed:")
+oggettixvaligia.reverse()
+print(oggettixvaligia)
+
+print("\nOriginal order:")
+oggettixvaligia.reverse()
+print(oggettixvaligia)
+
+print("\nAlphabetical")
+oggettixvaligia.sort()
+print(oggettixvaligia)
+
+print("\nReverse alphabetical")
+oggettixvaligia.sort(reverse=True)
+print(oggettixvaligia)
+
+print(f" Questa è la lista x fare la valigia.")
+x = len(oggettixvaligia)
+print(x)
+
---
+++ list3-6.py
@@ -0,0 +1,52 @@
+# Invite some people to dinner.
+guests = ['Padre Pio', 'Jesus', 'Albert Einstein']
+
+name = guests[0].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[1].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[2].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[1].title()
+print(f"\nSorry, {name} can't make it to dinner.")
+
+# Jack can't make it! Let's invite Elton John instead.
+del(guests[1])
+guests.insert(1, 'Elton John')
+
+# Print the invitations again.
+name = guests[0].title()
+print(f"\n{name}, please come to dinner.")
+
+name = guests[1].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[2].title()
+print(f"{name}, please come to dinner.")
+
+# We got a bigger table, so let's add some more people to the list.
+print("\nWe got a bigger table!")
+guests.insert(0, 'Rut Bertossa')
+guests.insert(2, 'reinhold messner')
+guests.append('Rinaldo Bertossa')
+
+name = guests[0].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[1].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[2].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[3].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[4].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[5].title()
+print(f"{name}, please come to dinner.")
