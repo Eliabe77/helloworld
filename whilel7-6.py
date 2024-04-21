a19f39bf0aa7338e2118a445dc603ab7feb7cfd2
Eliabe77 <158612069+Eliabe77@users.noreply.github.com>
2024-04-21 17:29:58 +0200

    Add files via upload

--- whilel7-6.py
+++ whilel7-6.py
@@ -1,20 +1,25 @@
 prompt = "How old are you?"
 prompt += "\n Thank you for your answer "
 
-while True:
-    age = input(prompt)
+c=1
+while c <= 10:
+    print("c" + str(c))
+c=c+1
+
+age = input(prompt)
     
-    age = int(age)
+age = int(age)
     
-    if age < 3:
+if age < 3:
         print("  You get in free!")
-    elif age < 13:
+elif age < 13:
          print("  Your ticket is $10.")
-    else:
-        print("  Your ticket is $15.")
+else:
+        print("  Your ticket is $15.") 
+             
+        
     
-    while age >="0":
-        break
+
     
 
     
